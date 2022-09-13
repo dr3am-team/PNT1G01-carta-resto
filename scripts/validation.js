fbf5a7938fe5546325583ac4d0d6c68a253baf32
Felix Lerner <felix.lerner01@gmail.com>
2022-09-12 23:07:56 -0300

    funcion de validar empty

--- scripts/validation.js
+++ scripts/validation.js
@@ -7,3 +7,15 @@ function ValidarNroTel(inputtxt) {
     return false;
   }
 }
+
+//chequear que no este vacio
+function required() {
+  var empt = document.forms["form1"]["text1"].value;
+  if (empt == "") {
+    alert("Please input a Value");
+    return false;
+  } else {
+    alert("Code has accepted : you can try another");
+    return true;
+  }
+}
